4e7c8dde07627013b1c18ec766d3d95e3e474b54
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-09 12:24:46 +0100

    build: using auto-discovery

--- build.hpp
+++ build.hpp
@@ -1,18 +1,8 @@
 #pragma once
 
-#include "../traits/build.hpp"
 #include "ecow.hpp"
 
 inline auto hai() {
   using namespace ecow;
-
-  auto m = unit::create<mod>("hai");
-  m->add_wsdep("traits", traits());
-  m->add_part("holder");
-  m->add_part("sptr");
-  m->add_part("uptr");
-  m->add_part("array");
-  m->add_part("varray");
-  m->add_part("cstr");
-  return m;
+  return unit::create<box>("hai");
 }
